b4801118270c81d328e76351f85dcb96f5e9746d
jschirad <jfacuscirado@gmail.com>
2020-10-10 13:47:38 +0200

    sa

--- Piscina 42/03/ex04/SuperTrap.cpp
+++ Piscina 42/03/ex04/SuperTrap.cpp
@@ -6,9 +6,48 @@
 /*   By: jschirad <jschirad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 18:08:18 by jschirad          #+#    #+#             */
-/*   Updated: 2020/09/20 18:14:07 by jschirad         ###   ########.fr       */
+/*   Updated: 2020/09/23 18:10:37 by jschirad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "SuperTrap.hpp"
 
+SuperTrap::SuperTrap(std::string const &name): NinjaTrap(name), FragTrap(name)
+{
+	std::cout << this->name << ": SUPER CREATING." << std::endl;
+	this->hitPoints = this->FragTrap::hitPoints;
+	this->maxHitPoints = this->FragTrap::maxHitPoints;
+	this->energyPoints = this->NinjaTrap::energyPoints;
+	this->maxEnergyPoints = this->NinjaTrap::maxEnergyPoints;
+	this->meleeAttackDamage = this->NinjaTrap::meleeAttackDamage;
+	this->rangedAttackDamage = this->FragTrap::rangedAttackDamage;
+	this->armorDamageReduction = this->FragTrap::armorDamageReduction;
+}
+
+SuperTrap::SuperTrap(SuperTrap const &other):
+	ClapTrap(obj.name), NinjaTrap(name), FragTrap(name)
+{
+	std::cout << this->name << ": SUPER CREATING." << std::endl;
+	ClapTrap::copy(other);
+}
+
+SuperTrap::~SuperTrap()
+{
+	std::cout << this->name << ": SUPER DESTROYING."  << std::endl;
+}
+
+SuperTrap &SuperTrap::operator=(SuperTrap const &other)
+{
+	ClapTrap::copy(other);
+	return (*this);
+}
+
+void SuperTrap::rangedAttack(std::string const &target)
+{
+	FragTrap::rangedAttack(target);
+}
+
+void SuperTrap::meleeAttack(std::string const &target)
+{
+	NinjaTrap::meleeAttack(target);
+}
--- Piscina 42/03/ex04/SuperTrap.hpp
+++ Piscina 42/03/ex04/SuperTrap.hpp
@@ -6,31 +6,31 @@
 /*   By: jschirad <jschirad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/20 18:08:07 by jschirad          #+#    #+#             */
-/*   Updated: 2020/09/20 18:14:05 by jschirad         ###   ########.fr       */
+/*   Updated: 2020/09/23 18:09:24 by jschirad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SUPERTRAP_HPP
-#define SUPERTRAP_HPP
+# define SUPERTRAP_HPP
 
-#include <iostream>
-#include <string.h>
-#include "FragTrap.hpp"
-#include "NinjaTrap.hpp"
+# include <iostream>
+# include <string>
+# include "NinjaTrap.hpp"
+# include "FragTrap.hpp"
 
-class SuperTrap : public NinjaTrap, FragTrap
+class SuperTrap: public NinjaTrap, public FragTrap
 {
 private:
 	SuperTrap();
 public:
 	SuperTrap(std::string const &name);
-	SuperTrap(SuperTrap const &obj);
-	~SuperTrap();
+	SuperTrap(SuperTrap const &other);
+	virtual ~SuperTrap();
 
-	SuperTrap &operator=(SuperTrap const &obj);
+	SuperTrap &operator=(SuperTrap const &other);
 
-	void	rangedAttack(std::string const &target);
-	void	meleeAttack(std::string const &target);
+	void rangedAttack(std::string const &target);
+	void meleeAttack(std::string const &target);
 };
 
 #endif
